f885e6dec8b9c06ce5292eb148c4b6e362ed2831
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2024-02-15 20:42:53 +0530

    creating basic backend with chai

--- lec1/index.js
+++ lec1/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 const port = 3000
-
+//3000 port hamare laptop me free hai but ho sakta hai ki user ka port 3000 busy ho there for we put it into .env file
+//jaha bhi use karna hai baha   process.env.var_name
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -15,6 +17,11 @@ app.get('/twitter' , (req , res) => {
     res.send('Hello Devendra welcome to twitter')
 })
 
+app.get('/youtube' , (req , res) => {
+    res.send('<h1>Enjoye Backend with chai</h1>')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+//sensitive information like(ID password of DB) we push these variable(like PORT) into .env file
